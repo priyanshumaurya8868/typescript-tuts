b38cb0e8540916a8cddc7601ffefa9751119cd7a
Priyanshu <priyanshumaurya.8868@gmail.com>
2022-08-23 04:18:23 +0530

    typecasting + nullcheck

--- app.ts
+++ app.ts
@@ -1,7 +1,15 @@
-function add(num1, num2) {
+const ip1 = document.getElementById("ip1") as HTMLInputElement;
+const ip2 = document.getElementById("ip2") as HTMLInputElement;  //typecasting
+const btn = document.querySelector("button");
+
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-console.log(add(1, 6));
+//null check
+btn?.addEventListener("click", (event) => {
+  const num1 = ip1.value;
+  const num2 = ip2.value;
 
-console.log(add('1', '6'));
+  console.log(add(+num1, +num2)); //changing str to num by '+'
+});
